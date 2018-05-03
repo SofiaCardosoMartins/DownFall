dd7f79c6d6af5bd634ff171b7d629751da2b0733
SofiaCardosoMartins <up201606033@g.uporto.pt>
2018-05-03 10:54:55 +0100

    Added the GameController class. First configurations in the desktop launcher.

---
+++ code/core/src/com/mygdx/game/controller/GameController.java
@@ -0,0 +1,13 @@
+package com.mygdx.game.controller;
+
+import com.badlogic.gdx.physics.box2d.World;
+
+public class GameController {
+
+    public static GameController instance;
+    public static final String TITLE = "DownFall";
+    public static final int WORLD_WIDTH = 1080;
+    public static final int WORLD_HEIGHT = 720;
+    public static final float GRAVITY = -9.8f;
+    //private final World world;
+}
--- code/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ code/desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -3,10 +3,16 @@ package com.mygdx.game.desktop;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
 import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
 import com.mygdx.game.DownFall;
+import com.mygdx.game.controller.GameController;
 
 public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+
+		config.title = GameController.TITLE;
+		config.width = GameController.WORLD_WIDTH;
+		config.height = GameController.WORLD_HEIGHT;
+
 		new LwjglApplication(new DownFall(), config);
 	}
 }
